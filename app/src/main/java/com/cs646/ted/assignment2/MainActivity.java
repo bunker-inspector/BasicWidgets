497fb159664e786644b5a78c37dca31873533ccb
Ted <tedgkassen@gmail.com>
2015-02-04 09:08:07 -0800

    initial commit

---
+++ app/src/main/java/com/cs646/ted/assignment2/MainActivity.java
@@ -0,0 +1,83 @@
+package com.cs646.ted.assignment2;
+
+import android.app.ListFragment;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.lang.reflect.Array;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.activities_array,
+                android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        if (savedInstanceState == null) {
+
+            getFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.container, new MainActivityFragment())
+                    .commit();
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public static class MainActivityFragment extends ListFragment {
+
+
+        /*@Override
+        public View onCreateView(LayoutInflater inflater,
+                                 ViewGroup container,
+                                 Bundle savedInstanceState)
+        {
+            ArrayAdapter<Object> adapter = new ArrayAdapter<Object>(getActivity(),
+                    android.R.layout.simple_list_item_1,
+                    0);
+            setListAdapter(adapter);
+
+            return super.onCreateView(inflater, container,savedInstanceState);
+        }*/
+    }
+
+}
